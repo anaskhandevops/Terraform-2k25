0168ac58f93b721849bc72e1a70b715b172fba6c
devanaskhanuae <dev.anaskhanuae@gmail.com>
2025-05-02 21:25:29 +0500

    aws-ec2 outputs.tf

---
+++ aws-ec2/outputs.tf
@@ -0,0 +1,45 @@
+# --- EC2 Instance Outputs ---
+
+output "instance_id" {
+  description = "The ID of the created EC2 instance."
+  value       = aws_instance.my_ec2.id
+}
+
+output "instance_arn" {
+  description = "The ARN of the created EC2 instance."
+  value       = aws_instance.my_ec2.arn
+}
+
+output "instance_public_ip" {
+  description = "The public IP address assigned to the EC2 instance (if applicable)."
+  # Note: This will be empty if the instance is in a private subnet without a public IP.
+  value       = aws_instance.my_ec2.public_ip
+}
+
+output "instance_private_ip" {
+  description = "The private IP address assigned to the EC2 instance."
+  value       = aws_instance.my_ec2.private_ip
+}
+
+output "instance_public_dns" {
+  description = "The public DNS name assigned to the EC2 instance (if applicable)."
+  # Note: This will be empty if the instance doesn't have a public IP/DNS.
+  value       = aws_instance.my_ec2.public_dns
+}
+
+output "instance_private_dns" {
+  description = "The private DNS name assigned to the EC2 instance."
+  value       = aws_instance.my_ec2.private_dns
+}
+
+# --- Security Group Outputs ---
+
+output "web_server_security_group_id" {
+  description = "The ID of the security group created for the web server."
+  value       = aws_security_group.web_server_sg.id
+}
+
+output "web_server_security_group_arn" {
+  description = "The ARN of the security group created for the web server."
+  value       = aws_security_group.web_server_sg.arn
+}
